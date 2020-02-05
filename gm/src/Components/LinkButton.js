65baa7d3c389faaaa36e2395c479a6fa31f0bf9c
Oswald BLASSOU <oswaldblassou@yahoo.fr>
2020-02-05 15:13:38 +0100

    [GM]  feat(mqqtClient): added link

--- gm/src/Components/LinkButton.js
+++ gm/src/Components/LinkButton.js
@@ -1,35 +1,16 @@
-import React, { Component } from 'react';
-import { Button } from '@material-ui/core'
-
-function publish() {
-    var mqtt = require('mqtt')
-
-    var settings = {
-        username: 'team14',
-        password: 'cniueargfe',
-
-      }
-      
-      
-    var client  = mqtt.connect('wss://mr1dns3dpz5mjj.messaging.solace.cloud:8443/', settings)
-    
-
-    client.on('connect', function () {
-        client.publish('team14/prod/city/reset', '')
-        console.log('publish reset')
-    })
-
-    client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString())
-    client.end()
-    })
-
-}
+import React from "react";
+import { Button } from "@material-ui/core";
 
 export default function LinkButton(props) {
-
-    return (
-        <Button style={{width: '20%'}} variant='contained' onClick={publish}> RESET</Button>
-    )
+  const { onClickFunction } = props;
+  return (
+    <Button
+      style={{ width: "20%" }}
+      variant="contained"
+      onClick={onClickFunction}
+    >
+      {" "}
+      RESET
+    </Button>
+  );
 }
