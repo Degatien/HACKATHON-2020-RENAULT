01b4ab8395c950aea7b71b77564d02b8f27098c0
archamm <matthieu.archambault@epita.fr>
2020-02-05 16:22:58 +0100

    [GM] update App

--- gm/src/App.js
+++ gm/src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LinkButton from './Components/LinkButton'
+import Teleport from './Components/Teleport'
+import ChangeTrafficCondition from './Components/ChangeTrafficCondition'
 import mqqtClient from './api/mqttClient'
-
+import OpenOrCloseSubway from './Components/OpenOrCloseSubway'
 function App() {
   return (
-    <div style={{ display:'flex', flexDirection:'column', }} >
+    <div style={{ display:'flex', flexDirection:'column', marginTop: '20px'}} >
       <LinkButton variant="full" onClickFunction={mqqtClient.reset()}/>
+      <Teleport/>
+      <ChangeTrafficCondition/>
+      <OpenOrCloseSubway/>
     </div>
   )
 }
