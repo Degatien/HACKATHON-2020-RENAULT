b43f6f349dee6b2b5a7607f5e029e6da1e415812
Alister Gracias <alister.gracias@epita.fr>
2020-02-05 13:48:47 +0100

    [API] Added Agent api

--- app/src/Api/Agent.js
+++ app/src/Api/Agent.js
@@ -1,1 +1,13 @@
-import 'react';
+import axios from 'axios';
+
+const baseUrl = "http://team14.xp65.renault-digital.com/api/agent";
+
+export function getSituation() {
+  return axios.get(baseUrl + "/api/user/situation/last")
+    .then(data => {
+      return data && data.data ? data.data : null;
+    })
+    .catch(error => {
+      throw new Error("Agent situation");
+    })
+}
